09dbf776291f36c646b67571d5d2e022bfe9ff9a
kammagik <kammagik@yahoo.com>
2019-06-09 22:40:54 -0400

    update to env file

---
+++ app/chatapp.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Chat extends Component {
+  render() {
+    return 
+    <div>
+        <h2>Hello World</h2>
+    </div>;
+  }
+}
+
+export default Chat;
